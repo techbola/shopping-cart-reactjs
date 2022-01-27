a4b468c95fe9927251467fafc2d0f9b4ed53eb27
Adebola <bfagbowore@gmail.com>
2022-01-27 01:34:29 +0100

    sort products, product rating filtering, and search product

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button, Form } from "react-bootstrap";
+import { CartState } from "../context/Context";
 import Rating from "./Rating";
 
 const Filters = () => {
-  const [rate, setRate] = useState(2);
+  const {
+    productState: { byStock, byFastDelivery, byRating, sort },
+    productDispatch,
+  } = CartState();
 
   return (
     <div className="filters">
@@ -15,6 +19,13 @@ const Filters = () => {
           name="group1"
           type="radio"
           id={`inline-1`}
+          onChange={() =>
+            productDispatch({
+              type: "SORT_BY_PRICE",
+              payload: "lowToHigh",
+            })
+          }
+          checked={sort === "lowToHigh" ? true : false}
         />
       </span>
       <span>
@@ -24,6 +35,13 @@ const Filters = () => {
           name="group1"
           type="radio"
           id={`inline-2`}
+          onChange={() =>
+            productDispatch({
+              type: "SORT_BY_PRICE",
+              payload: "highToLow",
+            })
+          }
+          checked={sort === "highToLow" ? true : false}
         />
       </span>
       <span>
@@ -33,6 +51,12 @@ const Filters = () => {
           name="group1"
           type="checkbox"
           id={`inline-3`}
+          onChange={() =>
+            productDispatch({
+              type: "FILTER_BY_STOCK",
+            })
+          }
+          checked={byStock}
         />
       </span>
       <span>
@@ -42,17 +66,37 @@ const Filters = () => {
           name="group1"
           type="checkbox"
           id={`inline-4`}
+          onChange={() =>
+            productDispatch({
+              type: "FILTER_BY_DELIVERY",
+            })
+          }
+          checked={byFastDelivery}
         />
       </span>
       <span>
         <label style={{ paddingRight: 10 }}>Rating: </label>
         <Rating
-          rating={rate}
-          onClick={(i) => setRate(i + 1)}
+          rating={byRating}
+          onClick={(i) =>
+            productDispatch({
+              type: "FILTER_BY_RATING",
+              payload: i + 1,
+            })
+          }
           style={{ cursor: "pointer" }}
         />
       </span>
-      <Button variant="light">Clear Filters</Button>
+      <Button
+        onClick={(i) =>
+          productDispatch({
+            type: "CLEAR_FILTER",
+          })
+        }
+        variant="light"
+      >
+        Clear Filters
+      </Button>
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,13 +7,46 @@ import "./styles.css";
 const Home = () => {
   const {
     state: { products },
+    productState: { byStock, byFastDelivery, byRating, sort, searchQuery },
   } = CartState();
+
+  const filteredProducts = () => {
+    let sortedProducts = products;
+
+    if (sort) {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        sort === "lowToHigh" ? a.price - b.price : b.price - a.price
+      );
+    }
+
+    if (!byStock) {
+      sortedProducts = sortedProducts.filter((prod) => prod.inStock);
+    }
+
+    if (byFastDelivery) {
+      sortedProducts = sortedProducts.filter((prod) => prod.fastDelivery);
+    }
+
+    if (byRating) {
+      sortedProducts = sortedProducts.filter(
+        (prod) => prod.ratings >= byRating
+      );
+    }
+
+    if (searchQuery) {
+      sortedProducts = sortedProducts.filter((prod) =>
+        prod.name.toLowerCase().includes(searchQuery)
+      );
+    }
+
+    return sortedProducts;
+  };
 
   return (
     <div className="home">
       <Filters />
       <div className="productContainer">
-        {products.map((product) => (
+        {filteredProducts().map((product) => (
           <SingleProduct product={product} key={product.id} />
         ))}
       </div>
